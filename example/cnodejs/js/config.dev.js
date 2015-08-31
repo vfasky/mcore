50692a9c781bd2e25dbf7adf693de945250ad510
vfasky <vfasky@gmail.com>
2015-08-31 23:02:34 +0800

    fix mcore url

--- example/cnodejs/js/config.dev.js
+++ example/cnodejs/js/config.dev.js
@@ -3,6 +3,6 @@ requirejs.config({
         "cnode": "./js/pack/cnode/1.0.0/dist/cnode.all",
         "tpl/cnode": "./js/tpl/cnode.240b72",
         "jquery": "//dn-cdnjscn.qbox.me/ajax/libs/jquery/2.1.4/jquery.min",
-        "mcore": "/dist/mcore.all.min"
+        "mcore": "../../dist/mcore.all.min"
     }
 });
--- example/cnodejs/js/config.js
+++ example/cnodejs/js/config.js
@@ -1,1 +1,1 @@
-requirejs.config({"paths":{"cnode":"./js/pack/cnode/1.0.0/dist/cnode.all.min.8c228b","tpl/cnode":"./js/tpl/cnode.240b72","jquery":"//dn-cdnjscn.qbox.me/ajax/libs/jquery/2.1.4/jquery.min","mcore":"/dist/mcore.all.min"}});
+requirejs.config({"paths":{"cnode":"./js/pack/cnode/1.0.0/dist/cnode.all.min.8c228b","tpl/cnode":"./js/tpl/cnode.240b72","jquery":"//dn-cdnjscn.qbox.me/ajax/libs/jquery/2.1.4/jquery.min","mcore":"../../dist/mcore.all.min"}});
